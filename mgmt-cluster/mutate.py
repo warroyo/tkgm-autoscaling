59303d115820748216b4160c322ba0548447414f
warroyo <warroyo7199008@gmail.com>
2025-03-06 17:20:46 -0700

    fix response

--- mgmt-cluster/mutate.py
+++ mgmt-cluster/mutate.py
@@ -12,13 +12,31 @@ def webhook():
     request_json = request.get_json()
     app.logger.debug(request_json)
     if not request_json:
-        return jsonify({"response": {"allowed": True}}), 200
+        return jsonify(
+            {
+                "apiVersion": request_json.get("apiVersion"),
+                "kind": request_json.get("kind"),
+                "response": {
+                    "uid": request_json["request"].get("uid"),
+                    "allowed": True
+                }
+            }
+        )
 
     cluster = request_json['request']['object']
     cluster_name = cluster['metadata']['name']
     if  "machineDeployments" not in cluster['spec']['topology']['workers']:
          app.logger.info(f"request for {cluster_name} did not container machine deployments") 
-         return jsonify({"response": {"allowed": True}}), 200
+         return jsonify(
+            {
+                "apiVersion": request_json.get("apiVersion"),
+                "kind": request_json.get("kind"),
+                "response": {
+                    "uid": request_json["request"].get("uid"),
+                    "allowed": True
+                }
+            }
+        )
     patch = []
     mds = cluster['spec']['topology']['workers']['machineDeployments']
     for index, pool in enumerate(mds):
@@ -30,14 +48,19 @@ def webhook():
                     {"op": "add", "path": f"/spec/topology/workers/machineDeployments/{index}/metadata/annotations/cluster.x-k8s.io/cluster-api-autoscaler-node-group-min-size", "value": pool['metadata']['labels']['cluster.x-k8s.io/cluster-api-autoscaler-node-group-min-size']},
                     {"op": "remove", "path": f"/spec/topology/workers/machineDeployments/{index}/replicas"}
                     ])
-            
-    return jsonify({
-        "response": {
-            "allowed": True,
-            "patchType": "JSONPatch",
-            "patch": json.dumps(patch).encode('utf-8').decode('utf-8')
+    return jsonify(
+        {
+            "apiVersion": request_json.get("apiVersion"),
+            "kind": request_json.get("kind"),
+            "response": {
+                "uid": request_json["request"].get("uid"),
+                "allowed": True,
+                "status": {"message": "configuring cluster for autoscaling"},
+                "patchType": "JSONPatch",
+                "patch": json.dumps(patch).encode('utf-8').decode('utf-8')
+            }
         }
-    }), 200
+    )
 
 if __name__ == '__main__':
     app.logger.info("starting autoscale mutating webhook")
